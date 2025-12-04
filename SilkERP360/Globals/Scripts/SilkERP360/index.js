95d204a42c1b63817ad123c606b09cff405145a8
Minhazul Arnob <minhazul.arnob@gmail.com>
2025-12-04 11:51:27 +0600

    page after login redisgn

--- SilkERP360/Globals/Scripts/SilkERP360/index.js
+++ SilkERP360/Globals/Scripts/SilkERP360/index.js
@@ -1,10 +1,10 @@
 ﻿//will store image object of employee after the UserProfile is returned
 
 $(document).ready(function () {
-//    $.ajaxSetup({
-//        global: true,
-//        type: "POST"
-//    });
+    //    $.ajaxSetup({
+    //        global: true,
+    //        type: "POST"
+    //    });
 
     $(document).ajaxStart(function () {
         //alert("STARTING AJAX");
@@ -43,7 +43,7 @@ $(document).ready(function () {
 
 
 
-//    });
+    //    });
 
 });
 
@@ -70,59 +70,59 @@ function ChangePassword() {
     }
     if (lcl_str_NewPassword1 === lcl_str_NewPassword2) {
         $.ajax(
-        {
-            type: "POST",
-            async: true,
-            contentType: "application/json; charset=utf-8",
-            global: false,
-            url: gbl_URL_Root1 + "/WebServices/UserServices.asmx/ChangePassword",
-            data: "{IP_str_Username :'" + lcl_str_Username + "',IP_str_OldPassword :'" + lcl_str_CurrentPassword + "',IP_str_NewPassword : '" + lcl_str_NewPassword1 + "'}",
-            dataType: "json", /// <reference path= />
-            success: function (response) {
-                //alert("WS SUCCESS");
-                //alert(xhr.responseText);
-                //var lcl_obj_WSResponseText = xhresponseText;
-                //var lcl_obj_WSResponse = new Object();
-                //lcl_obj_WSResponse = ($.parseJSON(lcl_obj_WSResponseText)).d;
-                var lcl_obj_WSResponse = response.d;
-                //        console.log(lcl_obj_WSResponse);
-                switch (lcl_obj_WSResponse.WebServiceExecutionStatus) {
-                    case 0:
-                        //Success
-                        //debugger;
-                        if (lcl_obj_WSResponse.BResponse == false) {
+            {
+                type: "POST",
+                async: true,
+                contentType: "application/json; charset=utf-8",
+                global: false,
+                url: gbl_URL_Root1 + "/WebServices/UserServices.asmx/ChangePassword",
+                data: "{IP_str_Username :'" + lcl_str_Username + "',IP_str_OldPassword :'" + lcl_str_CurrentPassword + "',IP_str_NewPassword : '" + lcl_str_NewPassword1 + "'}",
+                dataType: "json", /// <reference path= />
+                success: function (response) {
+                    //alert("WS SUCCESS");
+                    //alert(xhr.responseText);
+                    //var lcl_obj_WSResponseText = xhresponseText;
+                    //var lcl_obj_WSResponse = new Object();
+                    //lcl_obj_WSResponse = ($.parseJSON(lcl_obj_WSResponseText)).d;
+                    var lcl_obj_WSResponse = response.d;
+                    //        console.log(lcl_obj_WSResponse);
+                    switch (lcl_obj_WSResponse.WebServiceExecutionStatus) {
+                        case 0:
+                            //Success
+                            //debugger;
+                            if (lcl_obj_WSResponse.BResponse == false) {
+                                DisplayError(lcl_obj_WSResponse.Message);
+                            }
+                            else {
+                                $('.c_p_field').val('');
+                                DisplayInformation(lcl_obj_WSResponse.Message);
+                            }
+                            break;
+                        case -100:
+                            //error
+                            //debugger;
                             DisplayError(lcl_obj_WSResponse.Message);
-                        }
-                        else {
-                            $('.c_p_field').val('');
-                            DisplayInformation(lcl_obj_WSResponse.Message);
-                        }
-                        break;
-                    case -100:
-                        //error
-                        //debugger;
-                        DisplayError(lcl_obj_WSResponse.Message);
-                        break;
-                    case 2:
-                        //Critical Error
-                        DisplayError(lcl_obj_WSResponse.Message);
-                        break;
-                    case 3:
-                        //Critical Error
-                        DisplayError(lcl_obj_WSResponse.Message);
-                        break;
-                    case 4:
-                        //Critical Error
-                        DisplayError(lcl_obj_WSResponse.Message);
-                        break;
+                            break;
+                        case 2:
+                            //Critical Error
+                            DisplayError(lcl_obj_WSResponse.Message);
+                            break;
+                        case 3:
+                            //Critical Error
+                            DisplayError(lcl_obj_WSResponse.Message);
+                            break;
+                        case 4:
+                            //Critical Error
+                            DisplayError(lcl_obj_WSResponse.Message);
+                            break;
+                    }
+                }, /// <reference path= />
+                error: function (event, jqxhr, settings, exception) {
+                    //debugger;
+                    //alert("LOCAL AJAX ERROR");
+                    DisplayError("There seems to be some network or server problem!!!Please contact the administrator!!!");
                 }
-            }, /// <reference path= />
-            error: function (event, jqxhr, settings, exception) {
-                //debugger;
-                //alert("LOCAL AJAX ERROR");
-                DisplayError("There seems to be some network or server problem!!!Please contact the administrator!!!");
-            }
-        });
+            });
     }
     else {
         DisplayError("'New Password' and 'Reconfirmed Password' dont match!!!");
@@ -146,18 +146,18 @@ function Login() {
         DisplayError(lcl_str_Message);
         return lcl_b_FormValidated;
     }
-    
+
     //var lcl_str_WMData = "{IP_str_Username :'" + lcl_str_Username + "',IP_str_Password :'" + lcl_str_Password + "'}";
 
     //Remove the Global ajaxSuccess event handler
     //$(document).unbind('ajaxSuccess');
 
-//    $(document).ajaxSuccess(function (event, xhr, settings) {
-//       
+    //    $(document).ajaxSuccess(function (event, xhr, settings) {
+    //       
 
-//    });
+    //    });
     //Validated. Send AJAX Request
-   // AjaxCall(gbl_URL_Root + "/WebServices/UserServices.asmx", "Authenticate", lcl_str_WMData);
+    // AjaxCall(gbl_URL_Root + "/WebServices/UserServices.asmx", "Authenticate", lcl_str_WMData);
 
     $.ajax(
         {
@@ -171,11 +171,10 @@ function Login() {
             success: function (response) {
 
                 var lcl_obj_WSResponse = response.d;
-                
+
                 switch (lcl_obj_WSResponse.WebServiceExecutionStatus) {
                     case 0:
                         //Success
-                        //debugger;
                         var lcl_b_AuthenticUser = lcl_obj_WSResponse.BResponse;
                         if (lcl_b_AuthenticUser == false) {
                             DisplayError(lcl_obj_WSResponse.Message);
@@ -191,74 +190,64 @@ function Login() {
                                 window.location = lcl_obj_ModuleMenusCompany.HomeLink + "?Mod=" + lcl_obj_ModuleMenusCompany.ModuleCode;
                             }
                             else {
-                                //Check what modules are permitted for the User
-                                /******************************************************************/
-                                /****************************SET IMAGE*****************************/
-                                /******************************************************************/
-                                //lcl_obj_UserProfile.ImageData = "data:" + lcl_obj_UserProfile.ImageType + ";base64," + lcl_obj_UserProfile.ImageData;
                                 var m_obj_EmployeeImage = "data:" + lcl_obj_UserProfile.Image.ImageType + ";base64," + lcl_obj_UserProfile.Image.ImageData;
-                                //alert(m_obj_EmployeeImage);
 
-                                var lcl_str_UserProfileDisplayHTML =
-                                    "<div style='width:420px;margin:20px auto;padding:20px;border:2px solid #0078D7;border-radius:15px;background: linear-gradient(45deg, var(--bs-info-bg-subtle), #0600ffe6);color:white;font-family:serif !important;'>" +
-                                    "<table id='tblUserCredential' cellpadding='0' cellspacing='0' style='width:100%; border-collapse: collapse; background:transparent;'>" +
-                                    // Header
-                                    "<thead><th colspan='2' style='font-size:16px;font-weight:bold;padding:10px;text-align:center;background:transparent;color:white;'>" +
-                                    "Welcome " + lcl_obj_UserProfile.EmployeeName + "!" +
-                                    "</th></thead>" +
-                                    "<tbody><tr>" +
-                                    "<td colspan='2' style='text-align:center;padding:20px;'>" +
-                                    "<div style='padding:15px;border-radius:10px;display:inline-block;background:transparent;'>" +
-                                    "<img id='imgImage' style='width:180px;height:230px;border-radius:10px;margin-bottom:10px;' src='" + m_obj_EmployeeImage + "' />" +
-                                    "<p style='margin:5px 0;'><strong></strong> " + lcl_obj_UserProfile.EmployeeID + "</p>" +
-                                    "<p style='margin:5px 0;'><strong></strong> " + lcl_obj_UserProfile.EmployeeName + "</p>" +
-                                    "<p style='margin:5px 0;'><strong></strong> " + lcl_obj_UserProfile.Company.Name + "</p>" +
-                                    "</div>" +
-                                    "</td>" +
-                                    "</tr>" +
-                                    // Module Permissions Header
-                                    "<thead><th colspan='2' style='text-align:center;padding:10px;font-size:14px;font-weight:bold;background:transparent;color:black;'>MODULE PERMISSIONS</th></thead>" +
-                                    "<thead>" +
-                                    "<th style='text-align:center;padding:8px;background:#0078D7;color:white;'>MODULE</th>" +
-                                    "<th style='text-align:center;padding:8px;background:#0078D7;color:white;'>COMPANIES</th>" +
-                                    "</thead>";
+                                var lcl_str_UserProfileDisplayHTML = `
+                                                <div style="max-width:500px;margin:0 auto;font-family:'Times New Roman', Times, serif;">
+                                                    <!-- Welcome Text -->
+                                                    <p style="margin:5px 0;font-size:16px;font-weight:bold;text-align:center;color:#333;">Welcome ${lcl_obj_UserProfile.EmployeeName}</p>
 
-                                // Loop through modules
-                                $.each(lcl_obj_ModulesMenusCompanies, function (index, lcl_obj_ModuleMenuCompany) {
-                                    lcl_str_UserProfileDisplayHTML += "<tr style='border-bottom:1px solid #ddd;'>" +
-                                        "<td style='text-align:center;padding:8px;'><a style='color:#673ab7;font-weight:bold;text-decoration:none;' href='" + lcl_obj_ModuleMenuCompany.HomeLink + "?Mod=" + lcl_obj_ModuleMenuCompany.ModuleCode + "'>" +
-                                        lcl_obj_ModuleMenuCompany.ModuleName +
-                                        "</a></td>";
+                                                    <!-- Employee Card Frame with Soft Background -->
+                                                    <div style="background: #e0f0ff; padding:10px; text-align:center; box-shadow:0 2px 6px rgba(0,0,0,0.1); border-radius:8px; margin-bottom:10px; color:#333;">
+                                                        <!-- Employee Image -->
+                                                        <img src="${m_obj_EmployeeImage}" alt="Employee Image" style="width:30%;object-fit:contain;margin-bottom:10px; border-radius:6px;"/>
 
-                                    var lcl_str_Company = "";
-                                    $.each(lcl_obj_ModuleMenuCompany.Companys, function (index1, lcl_obj_CompanyCore) {
-                                        lcl_str_Company += lcl_obj_CompanyCore.Name + "<br/>";
-                                    });
+                                                        <!-- Employee Info under Image -->
+                                                        <div>
+                                                            <p style="margin:2px 0;font-weight:bold;font-size:14px;">ID: ${lcl_obj_UserProfile.EmployeeID}</p>
+                                                            <p style="margin:2px 0;font-weight:bold;font-size:15px;">${lcl_obj_UserProfile.EmployeeName}</p>
+                                                            <p style="margin:2px 0;font-size:13px;">${lcl_obj_UserProfile.Company.Name}</p>
+                                                        </div>
+                                                    </div>
 
-                                    lcl_str_UserProfileDisplayHTML += "<td style='text-align:center;padding:8px;color:#673ab7'>" + lcl_str_Company + "</td></tr>";
-                                });
+                                                    <!-- Module Permissions Table -->
+                                                    <div style="max-height:200px;overflow-y:auto;border:1px solid #ccc;border-radius:6px;">
+                                                        <table style="width:100%;border-collapse:collapse;text-align:center;font-size:13px;">
+                                                            <thead style="position:sticky;top:0;background:#d9eaff;">
+                                                                <tr>
+                                                                    <th style="padding:6px;border-bottom:1px solid #ccc;">Module</th>
+                                                                    <th style="padding:6px;border-bottom:1px solid #ccc;">Companies</th>
+                                                                </tr>
+                                                            </thead>
+                                                            <tbody>`;
 
-                                lcl_str_UserProfileDisplayHTML += "</tbody></table></div>";
+                                                                                // Add module rows with subtle alternate colors
+                                                                                $.each(lcl_obj_ModulesMenusCompanies, function (index, lcl_obj_ModuleMenuCompany) {
+                                                                                    var lcl_str_Company = "";
+                                                                                    $.each(lcl_obj_ModuleMenuCompany.Companys, function (index1, lcl_obj_CompanyCore) {
+                                                                                        lcl_str_Company += lcl_obj_CompanyCore.Name + "<br/>";
+                                                                                    });
 
-                                // Inject into page
-                                $("#userProfileContainer").html(lcl_str_UserProfileDisplayHTML);
+                                                                                    var rowBackground = index % 2 === 0 ? "#f0f8ff" : "#ffffff"; // alternating row colors
 
+                                                                                    lcl_str_UserProfileDisplayHTML += `
+                                                        <tr style="background:${rowBackground};">
+                                                            <td style="padding:6px;border-bottom:1px solid #eee;">
+                                                                <a href="${lcl_obj_ModuleMenuCompany.HomeLink}?Mod=${lcl_obj_ModuleMenuCompany.ModuleCode}" style="color:#394860;text-decoration:none;font-weight:500;">
+                                                                    ${lcl_obj_ModuleMenuCompany.ModuleName}
+                                                                </a>
+                                                            </td>
+                                                            <td style="padding:6px;border-bottom:1px solid #eee;">${lcl_str_Company}</td>
+                                                        </tr>
+                                                    `;
+                                                                                });
 
-                                // Inject into page
-                                $("#userProfileContainer").html(lcl_str_UserProfileDisplayHTML);
-
-
-                                // Inject into page
-                                $("#userProfileContainer").html(lcl_str_UserProfileDisplayHTML);
-
-
-                                // Inject into page
-                                $("#userProfileContainer").html(lcl_str_UserProfileDisplayHTML);
-
-
-                                /******************************************************************/
-                                /******************************************************************/
-                                /******************************************************************/
+                                                                                lcl_str_UserProfileDisplayHTML += `
+                                                            </tbody>
+                                                        </table>
+                                                    </div>
+                                                </div>
+                                                `;
 
                                 if (lcl_obj_ModulesMenusCompanies == null) {
                                     DisplayNotification(lcl_str_UserProfileDisplayHTML); // "Your User Credential Has Been Authenticated!Your Module Access Permissions Has either been Revoked or Not Yet Been granted!Contact SSL Support!!!");
